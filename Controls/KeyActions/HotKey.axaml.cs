0cfac5c4ec21b2ca74d809c8c2fe6b6c759bcfcd
Timo Hauser <timohauser1212@web.de>
2025-09-01 03:36:14 +0200

    improved hotkey functionality so it can capture any possible hotkeys

--- Controls/KeyActions/HotKey.axaml.cs
+++ Controls/KeyActions/HotKey.axaml.cs
@@ -1,23 +1,31 @@
+﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using StreamDeckConfiguration.Controls;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace StreamDeckConfiguration;
 
-public partial class HotKey : KeyActionUserControl
+public partial class HotKey : KeyActionUserControl, INotifyPropertyChanged
 {
 	private bool waitingForShortcut;
 	private bool setSCFromDisplay = true;
+	private IntPtr hook = IntPtr.Zero;
+	private HookProc? proc;
+	private readonly HashSet<uint> downVKs = new();
 
 	public HotKey()
 	{
 		InitializeComponent();
 		DataContext = this;
+
 		setSCFromDisplay = false;
 		ShortcutDisplay = "Click to set shortcut";
 		setSCFromDisplay = true;
@@ -26,9 +34,15 @@ public partial class HotKey : KeyActionUserControl
 	protected override void OnInitialized()
 	{
 		base.OnInitialized();
-
 		var button = this.FindControl<Button>("MyShortcutButton");
+		button.Click += Button_Click;
 		button.AddHandler(KeyDownEvent, Button_KeyDown, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+	}
+
+	protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+	{
+		StopHook();
+		base.OnDetachedFromVisualTree(e);
 	}
 
 	private string shortcutDisplay = "";
@@ -41,72 +55,240 @@ public partial class HotKey : KeyActionUserControl
 			{
 				shortcutDisplay = value;
 				OnPropertyChanged();
-
 				if (setSCFromDisplay)
 				{
 					try
 					{
-						if (value != null)
-						{
+						if (!string.IsNullOrWhiteSpace(value))
 							Shortcut = KeyGesture.Parse(value);
-						}
 					}
-					catch { };
+					catch { }
 				}
 			}
 		}
 	}
 
-	public KeyGesture? Shortcut { get; set; }
+	public KeyGesture? Shortcut { get; private set; }
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected void OnPropertyChanged([CallerMemberName] string? name = null)
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-	private bool IsModifierOnly(Key key) =>
+	private static bool IsModifierOnly(Key key) =>
 		key is Key.LeftCtrl or Key.RightCtrl or
 			   Key.LeftShift or Key.RightShift or
 			   Key.LeftAlt or Key.RightAlt or
 			   Key.LWin or Key.RWin;
 
-	private string FormatShortcut(KeyModifiers modifiers, Key? key)
+	private static string FormatShortcut(KeyModifiers modifiers, Key? key)
 	{
 		var sb = new StringBuilder();
 		if (modifiers.HasFlag(KeyModifiers.Control)) sb.Append("Ctrl + ");
 		if (modifiers.HasFlag(KeyModifiers.Shift)) sb.Append("Shift + ");
 		if (modifiers.HasFlag(KeyModifiers.Alt)) sb.Append("Alt + ");
 		if (modifiers.HasFlag(KeyModifiers.Meta)) sb.Append("Win + ");
-		if (key is not null and not Key.None)
-			sb.Append(key.Value.ToString());
+		if (key is not null and not Key.None) sb.Append(key.Value.ToString());
 		return sb.ToString().TrimEnd(' ', '+');
 	}
 
-	private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+	private void Button_Click(object? sender, RoutedEventArgs e)
 	{
 		waitingForShortcut = true;
 		setSCFromDisplay = false;
 		ShortcutDisplay = "Waiting for shortcut...";
 		setSCFromDisplay = true;
+
+		StartHook();
 		Focus();
 	}
 
-	private void Button_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+	private void FinishCapture(KeyModifiers mods, Key key)
 	{
-		e.Handled = true;
-		if (!waitingForShortcut)
-			return;
-
-		var modifiers = e.KeyModifiers;
-		var key = e.Key;
-
-		if (key == Key.None || IsModifierOnly(key))
-		{
-			setSCFromDisplay = false;
-			ShortcutDisplay = FormatShortcut(modifiers, null);
-			setSCFromDisplay = true;
-			return;
-		}
-
-		Shortcut = new KeyGesture(key, modifiers);
+		Shortcut = new KeyGesture(key, mods);
 		setSCFromDisplay = false;
 		ShortcutDisplay = Shortcut.ToString();
 		setSCFromDisplay = true;
+
 		waitingForShortcut = false;
+		StopHook();
+	}
+
+	private void Button_KeyDown(object? sender, KeyEventArgs e)
+	{
+		if (waitingForShortcut)
+		{
+			e.Handled = true;
+			return;
+		}
+	}
+
+	// ==========================
+	//       Keyboard Hook
+	// ==========================
+	private void StartHook()
+	{
+		if (hook != IntPtr.Zero) return;
+		proc = HookCallback;
+		var hMod = GetModuleHandle(null);
+		hook = SetWindowsHookEx(WH_KEYBOARD_LL, proc, hMod, 0);
+		if (hook == IntPtr.Zero)
+			throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+		downVKs.Clear();
+	}
+
+	private void StopHook()
+	{
+		if (hook == IntPtr.Zero) return;
+		UnhookWindowsHookEx(hook);
+		hook = IntPtr.Zero;
+		proc = null;
+		downVKs.Clear();
+	}
+
+	private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+	{
+		if (nCode < 0)
+			return CallNextHookEx(hook, nCode, wParam, lParam);
+
+		var msg = (KeyboardMessage)wParam;
+		var data = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
+		uint vk = data.vkCode;
+
+		bool isDown = msg is KeyboardMessage.WM_KEYDOWN or KeyboardMessage.WM_SYSKEYDOWN;
+		bool isUp = msg is KeyboardMessage.WM_KEYUP or KeyboardMessage.WM_SYSKEYUP;
+
+		if (isDown) downVKs.Add(vk);
+		if (isUp) downVKs.Remove(vk);
+
+		if (waitingForShortcut)
+		{
+			var mods = KeyModifiers.None;
+			if (downVKs.Contains(0x10) || downVKs.Contains(0xA0) || downVKs.Contains(0xA1)) mods |= KeyModifiers.Shift;
+			if (downVKs.Contains(0x11) || downVKs.Contains(0xA2) || downVKs.Contains(0xA3)) mods |= KeyModifiers.Control;
+			if (downVKs.Contains(0x12) || downVKs.Contains(0xA4) || downVKs.Contains(0xA5)) mods |= KeyModifiers.Alt;
+			if (downVKs.Contains(0x5B) || downVKs.Contains(0x5C)) mods |= KeyModifiers.Meta;
+
+			Key? mainKey = TryMapVKToAvaloniaKey(vk, out var mapped) && !IsModifierOnly(mapped)
+				? mapped
+				: FirstNonModifierFromSet(downVKs);
+
+			Dispatcher.UIThread.Post(() =>
+			{
+				setSCFromDisplay = false;
+				ShortcutDisplay = FormatShortcut(mods, mainKey is { } mk ? mk : null);
+				setSCFromDisplay = true;
+			});
+
+			if (isDown && mainKey is { } realKey && !IsModifierOnly(realKey))
+			{
+				Dispatcher.UIThread.Post(() => FinishCapture(mods, realKey));
+			}
+
+			return (IntPtr)1;
+		}
+
+		return CallNextHookEx(hook, nCode, wParam, lParam);
+	}
+
+	private static Key? FirstNonModifierFromSet(HashSet<uint> vks)
+	{
+		foreach (var v in vks)
+		{
+			if (TryMapVKToAvaloniaKey(v, out var k) && !IsModifierOnly(k))
+				return k;
+		}
+		return null;
+	}
+
+	private static bool TryMapVKToAvaloniaKey(uint vk, out Key key)
+	{
+		// A-Z
+		if (vk >= 0x41 && vk <= 0x5A)
+		{
+			key = Key.A + (int)(vk - 0x41);
+			return true;
+		}
+		// 0-9
+		if (vk >= 0x30 && vk <= 0x39)
+		{
+			key = Key.D0 + (int)(vk - 0x30);
+			return true;
+		}
+		// F1-F24
+		if (vk >= 0x70 && vk <= 0x87)
+		{
+			key = Key.F1 + (int)(vk - 0x70);
+			return true;
+		}
+
+		key = vk switch
+		{
+			0x1B => Key.Escape,
+			0x09 => Key.Tab,
+			0x0D => Key.Enter,
+			0x20 => Key.Space,
+			0x25 => Key.Left,
+			0x26 => Key.Up,
+			0x27 => Key.Right,
+			0x28 => Key.Down,
+			0x2E => Key.Delete,
+			0x2D => Key.Insert,
+			0x24 => Key.Home,
+			0x23 => Key.End,
+			0x21 => Key.PageUp,
+			0x22 => Key.PageDown,
+			0x6A => Key.Multiply,
+			0x6B => Key.Add,
+			0x6D => Key.Subtract,
+			0x6E => Key.Decimal,
+			0x6F => Key.Divide,
+
+			// Modifier
+			0x10 or 0xA0 => Key.LeftShift,
+			0xA1 => Key.RightShift,
+			0x11 or 0xA2 => Key.LeftCtrl,
+			0xA3 => Key.RightCtrl,
+			0x12 or 0xA4 => Key.LeftAlt,
+			0xA5 => Key.RightAlt,
+			0x5B => Key.LWin,
+			0x5C => Key.RWin,
+
+			_ => Key.None
+		};
+		return key != Key.None;
+	}
+
+	// --- Win32 Interop ---
+	private const int WH_KEYBOARD_LL = 13;
+
+	private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+	[DllImport("user32.dll")]
+	private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+	[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern IntPtr GetModuleHandle(string? lpModuleName);
+
+	private enum KeyboardMessage : int
+	{
+		WM_KEYDOWN = 0x0100,
+		WM_KEYUP = 0x0101,
+		WM_SYSKEYDOWN = 0x0104,
+		WM_SYSKEYUP = 0x0105
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct KBDLLHOOKSTRUCT
+	{
+		public uint vkCode;
+		public uint scanCode;
+		public uint flags;
+		public uint time;
+		public IntPtr dwExtraInfo;
 	}
 }
